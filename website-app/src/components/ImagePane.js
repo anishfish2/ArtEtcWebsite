b6ce8b3c3dbc1a5a436c66bff65597837a9f59d1
anishfish2 <anishkarthik21@gmail.com>
2023-03-06 01:03:08 -0600

    Added Image Pane and Components

---
+++ website-app/src/components/ImagePane.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ImagePane() {
+    return (
+        <div width = "1000" className='bg-[#703C3C] h-96 text-center flex bg-contain relative content-center'>
+            <div class = "carousel">
+                <img class = "slidingImage" alt="description of " src = "/Users/anishkarthik/Documents/Projects/ArtEtc/ArtEtcWebsite/website-app/images/anishpfp.webp"></img>
+                <img class = "slidingImage" alt="description of " src = "././images/IMG-3829.jpg"></img>
+                <img class = "slidingImage" alt="description of " src = "././images/tpose.jpeg"></img>
+                <img class = "slidingImage" alt="description of " src = "././images/unnamed.jpeg"></img>
+                HELSINFLSIDFNSLDIFNLSDINF
+            </div>
+        
+                
+        </div>
+    )
+}
---
+++ website-app/src/components/Join.js
@@ -0,0 +1,26 @@
+
+import React from 'react'
+
+export default function Join() {
+    return (
+        <div width = "1000" className='bg-[#FFF0D2] text-center flex bg-contain relative content-center'>
+            <div className='pt-32 pl-32' >
+                <div className='bg-[#FFF0D2] w-40 h-40 relative border-4 border-[#895235] font-custom text-center flex items-center justify-center'>
+                    <div className = 'mx-8'>
+                        Traveling Sketchbook info pogchampion
+                    </div>
+                </div>
+            </div>
+
+            <div className = "pt-12 pb-12 pl-72 ">
+                <div className = "bg-[#B67B58] flex bg-contain relative" height = "1000">
+                    <div>
+                        <iframe title = "asdfsdf" src="https://calendar.google.com/calendar/embed?src=artetc.tamu%40gmail.com&ctz=America%2FChicago" className = "border-[#895235]"  frameborder="0" scrolling="no"></iframe>
+
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
